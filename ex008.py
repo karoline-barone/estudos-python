44df79bfd8c77e41643849ccef2454584f38329b
Karoline Barone Semolini <karol.barone@hotmail.com>
2026-04-23 10:26:59 -0300

    Conversor de medidas

---
+++ ex008.py
@@ -0,0 +1,3 @@
+#escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros
+metros = float(input('Digite a quantidade de metros: '))
+print(f'{metros} metros equivale a {metros*100} centímetros e {metros*1000} milímetros')
