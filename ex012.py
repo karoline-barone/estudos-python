ec47e3d4110595c54cae157e8dd45fc54a0877d7
Karoline Barone Semolini <karol.barone@hotmail.com>
2026-04-23 11:14:08 -0300

    Calculando descontos

---
+++ ex012.py
@@ -0,0 +1,3 @@
+#leia o valor de um produto e mostre seu novo valor com 5% de desconto
+valor = float(input('Qual o valor do produto em reais?'))
+print(f'O valor do produto com desconto é R${valor*0.95:.2f}')
