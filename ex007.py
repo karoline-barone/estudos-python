78e26a8cdf8c5af478b4d4b49f355146024cb4b9
Karoline Barone Semolini <karol.barone@hotmail.com>
2026-04-23 10:20:01 -0300

    Média aritmética

---
+++ ex007.py
@@ -0,0 +1,4 @@
+#desenvolva um programa que leia duas notas de um aluno, calcule e mostre a sua média
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+print(f'A média das notas é {(n1+n2)/2:.1f}')
