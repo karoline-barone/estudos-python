ed244fef56ac97fab314e90846307b42ae03468f
Karoline Barone Semolini <karol.barone@hotmail.com>
2026-04-23 10:12:19 -0300

    Dobro, triplo e raiz quadrada

---
+++ ex006.py
@@ -0,0 +1,3 @@
+#crie um algoritmo que leia um numero e mostre o seu dobro, triplo e raiz quadrada
+n = int(input("Digite um numero inteiro: "))
+print(f'O dobro de {n} é {n*2}, o triplo é {n*3} e a raiz quadrada {n**(1/2)}')
