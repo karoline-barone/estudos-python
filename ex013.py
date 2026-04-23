7d35fa928e5c1fecec5db11d442761793ee0deef
Karoline Barone Semolini <karol.barone@hotmail.com>
2026-04-23 11:18:16 -0300

    Reajuste salarial

---
+++ ex013.py
@@ -0,0 +1,3 @@
+#leia o salário de um funcionário e mostre seu novo salário com 15% de aumento
+salario = float(input('Qual o salário em reais? '))
+print(f'O novo salário com 15% de aumento é R${salario*1.15:.2f}')
