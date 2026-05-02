404874ad9b492e23f7a0b1fb9488b479c2fe9d08
Karoline Barone Semolini <karol.barone@hotmail.com>
2026-05-02 07:00:52 -0300

    Par ou impar?

---
+++ ex030.py
@@ -0,0 +1,5 @@
+#leia um numero inteiro e mostre na tela se é par ou ímpar
+num = int(input('Digite um número inteiro:  '))
+if num % 2 == 0:
+    print(f'O número {num} é par!')
+else: print (f'O número {num} é impar!')
