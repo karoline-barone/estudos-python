f9fbe040e77ea792739726a4ea3afe7993feedcf
Karoline Barone Semolini <karol.barone@hotmail.com>
2026-05-02 07:16:06 -0300

    Ano bissexto

---
+++ ex032.py
@@ -0,0 +1,5 @@
+#leia um ano qualquer e mostre ele ele é bissexto
+ano = int(input('Digite um ano qualquer: '))
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f'O ano {ano} é bissexto!')
+else: print(f'O ano {ano} não é bissexto!')
