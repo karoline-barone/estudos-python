bda4c1fb930de246a0feb14f7b4bdf99d7b75c9e
Karoline <karol.barone@hotmail.com>
2026-06-29 18:25:34 -0300

    Ex066 - Vários números com flag

---
+++ fundamentos/ex066.py
@@ -0,0 +1,11 @@
+'''Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o
+valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles
+(desconsiderando o flag).'''
+num = cont = soma = 0
+while True:
+    num = int(input('Digite um número inteiro [999 para parar]: '))
+    if num == 999:
+        break
+    cont += 1
+    soma += num
+print(f'Você digitou {cont} números e a soma deles é {soma}.')
