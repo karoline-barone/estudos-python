a03fc488d0eb138e6603b2037c6131e3a937d7f8
Karoline <karol.barone@hotmail.com>
2026-07-15 09:56:13 -0300

    Ex074 - Maior e menor valores em tupla

---
+++ fundamentos/ex074.py
@@ -0,0 +1,6 @@
+#Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
+from random import randint
+numeros = (randint(1,20), randint(1,20), randint(1,20), randint(1,20), randint(1,20))
+print(f'Os valores sorteados foram: {numeros}.')
+print(f'O maior valor sorteado foi:{max(numeros)}.')
+print(f'O menor valor sorteado foi:{min(numeros)}.')
