aaa95c2a39ff15c4e1588b8cc29440ffb9e0f8fe
Karoline <karol.barone@hotmail.com>
2026-08-21 11:46:13 -0300

    Desafio003 - criador da Smalltalk

---
+++ POO/desafio003.py
@@ -0,0 +1,11 @@
+''' Desafio 003 - Quem foi o criador da Smalltalk, uma das primeiras linguagens OO?
+[ ] a) Edsger Dijkstra
+[ ] b) Guido Van Rossum
+[X] c) Alan Kay
+[ ] d) Kristen Nygaard
+
+Explicação: Alan Kay liderou a equipe que desenvolveu a linguagem Smalltalk na década
+de 1970 no centro de pesquisas Xerox PARC. Ele é amplamente considerado um dos pais da
+Orientação a Objetos e foi quem cunhou o próprio termo "Object-Oriented Programming"
+(Programação Orientada a Objetos).
+'''
