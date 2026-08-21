6cd937da5b7b42e25f34b1bd09005d5e79ca2081
Karoline <karol.barone@hotmail.com>
2026-08-21 11:28:28 -0300

    Desafio002 - Primeiras linguagens lineares

---
+++ POO/desafio002.py
@@ -0,0 +1,12 @@
+''' Desafio 002 - Qual das características a seguir identifica as primeiras linguagens lineares?
+[ ] a) Maior modularidade
+[X] b) Desvios forçados com GOTO
+[ ] c) Estruturas de controle
+[ ] d) Instruções de baixo nível
+
+Explicação: As primeiras linguagens de programação seguiam um fluxo estritamente linear
+(de cima para baixo). Para alterar o fluxo de execução ou repetir blocos de código,
+elas dependiam de desvios forçados usando o comando GOTO (ir para) apontando para
+números de linhas específicos. Isso frequentemente resultava no que chamamos de
+"código espaguete", que era extremamente difícil de ler, manter e depurar.
+'''
