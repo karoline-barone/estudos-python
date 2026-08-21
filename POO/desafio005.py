5dfa1cba0a086334fe2b65c96fc9c96475c5d658
Karoline <karol.barone@hotmail.com>
2026-08-21 12:13:06 -0300

    Desafio005 - OOAD

---
+++ POO/desafio005.py
@@ -0,0 +1,9 @@
+'''Desafio 005 - Na sigla OOAD, as últimas duas letras significam...
+[ ] a) Algol / Dashboard
+[ ] b) Algorithms / Digital
+[ ] c) Analytics / Default
+[X] d) Analysis / Design
+
+Explicação: OOAD significa "Object-Oriented Analysis and Design" (Análise e Projeto
+Orientados a Objetos).
+'''
