e5e52b8390b7b968de50af0fa63d9613b2d3692c
Karoline <karol.barone@hotmail.com>
2026-08-21 11:15:59 -0300

    Desafio001 - Crise do software

---
+++ POO/desafio001.py
@@ -0,0 +1,14 @@
+'''Desafio 001 - Em que década aconteceu a "Crise do Software" no mercado?
+[ ] a)1950
+[X] b)1960
+[ ] c)1980
+[ ] d)2000
+
+Contexto:
+O termo "Crise do Software" foi cunhado oficialmente em 1968, durante a conferência da OTAN realizada na Alemanha.
+Ela aconteceu porque, na década de 1960, o hardware evoluiu muito rápido com a chegada dos circuitos integrados,
+permitindo que as empresas tentassem criar sistemas muito mais complexos. Como as pessoas programavam sem uma
+metodologia padrão, quase todos os projetos sofriam com:
+- Estouro gritante de orçamento
+- Atrasos imensos na entrega
+- Softwares cheios de bugs e difíceis de manter'''
