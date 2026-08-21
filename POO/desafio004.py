f961a1ba5786d4761d4dd17b05df2ddfcbf6607f
Karoline <karol.barone@hotmail.com>
2026-08-21 11:58:37 -0300

    Desafio004 - Linguagem Simula

---
+++ POO/desafio004.py
@@ -0,0 +1,10 @@
+''' Desafio 004 - A linguagem Simula foi um superconjunto de que outra linguagem?
+[X] a) ALGOL
+[ ] b) C
+[ ] c) Smalltalk
+[ ] d) Python
+
+Explicação: A linguagem Simula foi criada como um superconjunto da linguagem ALGOL 60.
+Os criadores adicionaram os conceitos de classes, objetos, herança e subclasses à base sintática do ALGOL,
+introduzindo pela primeira vez na história o paradigma que hoje conhecemos como Orientação a Objetos.
+'''
