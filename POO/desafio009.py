2e34fafbbc849af096d4329e40a355a6c31a09bd
Karoline <karol.barone@hotmail.com>
2026-08-24 08:44:49 -0300

    Desafio009 - Natural

---
+++ POO/desafio009.py
@@ -0,0 +1,9 @@
+'''Desafio 009 - O código fica muito simples de ler e de compreender, já que não preciso saber os detalhes de funcionamento ao programar.
+[X] a) Natural
+[ ] b) Extensível
+[ ] c) Manutenível
+[ ] d) Oportuno
+
+Explicação: A vantagem descrita é o fato da POO ser natural. Ela aproxima a programação do mundo real, tornando o código mais intuitivo
+e legível. Além disso, ao utilizar objetos, focamos no o que o objeto faz e não em como ele faz internamente (conceito ligado à abstração
+e encapsulamento), simplificando a compreensão geral do sistema.'''
