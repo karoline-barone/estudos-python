c3aee79ee87a46971340ba6396f23f983fc804b3
Karoline <karol.barone@hotmail.com>
2026-08-24 08:50:53 -0300

    Desafio011 - Objeto abstrato

---
+++ POO/desafio011.py
@@ -0,0 +1,9 @@
+'''Desafio 011 - Qual dos objetos abaixo seria o único abstrato da lista?
+[ ] a) Sensor
+[ ] b) Robô
+[X] c) Notificação
+[ ] d) Livro
+
+Explicação: Em modelagem de sistemas orientados a objetos, diferenciamos coisas físicas (tangíveis) de conceitos abstratos
+(intangíveis). Sensor, Robô e Livro representam coisas que existem fisicamente no mundo real. Já uma Notificação é um conceito
+abstrato (um evento, uma mensagem ou um aviso), tornando-se o único objeto puramente intangível da lista.'''
