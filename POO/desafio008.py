a90daa7ca29a5ad9e3c9d5a1c7e8b68a458c713d
Karoline <karol.barone@hotmail.com>
2026-08-24 08:38:00 -0300

    Desafio008 - Oportuno

---
+++ POO/desafio008.py
@@ -0,0 +1,9 @@
+'''Desafio 008 - Posso desenvolver várias funções que dependam uma da outra ao mesmo tempo se o planejamento for seguido.
+[X] a) Oportuno
+[ ] b) Reutilizável
+[ ] c) Confiável
+[ ] d) Manutenível
+
+Explicação: O cenário trata da característica de ser oportuno. Na POO, devido à separação clara de responsabilidades em
+classes e objetos bem definidos no planejamento, diferentes desenvolvedores podem trabalhar em partes distintas do sistema
+simultaneamente, otimizando o tempo de entrega.'''
