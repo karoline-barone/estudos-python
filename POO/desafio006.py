93e744f6d0b611e058b6be5a2a5a9ee620654b2d
Karoline <karol.barone@hotmail.com>
2026-08-24 08:31:01 -0300

    Desafio006 - Reutilizar

---
+++ POO/desafio006.py
@@ -0,0 +1,9 @@
+'''Desafio 006 - Criou um sistema para Escola, ao criar outro sistema para Academia, parte do trabalho já estará feita,
+pois tenho Alunos nos dois.
+[ ] a) Confiável
+[ ] b) Natural
+[ ] c) Manutenível
+[X] d) Reutilizável
+
+Explicação: O cenário descreve a reutilização de código (reutilizabilidade). Ao criar uma classe Aluno no sistema da escola,
+esse mesmo código pode ser reaproveitado no sistema da academia, evitando retrabalho e economizando tempo de desenvolvimento.'''
