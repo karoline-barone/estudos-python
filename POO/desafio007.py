c6a1d311f3de95ca517acd3717cd64e2dbe1988d
Karoline <karol.barone@hotmail.com>
2026-08-24 08:34:27 -0300

    Desafio007 - Extensível

---
+++ POO/desafio007.py
@@ -0,0 +1,9 @@
+'''Desafio 007 - Se precisar adicionar uma nova funcionalidade ao sistema, consigo fazer isso com grande facilidade.
+[ ] a) Natural
+[X] b) Extensível
+[ ] c) Reutilizável
+[ ] d) Oportuno
+
+Explicação: A facilidade de adicionar novas funcionalidades ou expandir o sistema é chamada de extensibilidade.
+Na Programação Orientada a Objetos, isso é alcançado através de pilares como o polimorfismo e a herança, permitindo que
+o sistema cresça sem a necessidade de reescrever o código existente.'''
