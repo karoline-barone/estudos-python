db860be2c01a174e55b546ea1937dbb72f3ee115
Karoline <karol.barone@hotmail.com>
2026-08-24 08:47:46 -0300

    Desafio010 - Confiável

---
+++ POO/desafio010.py
@@ -0,0 +1,9 @@
+'''Desafio 010 - Posso alterar como uma funcionalidade trabalha e tudo continua funcionando como antes.
+[ ] a) Oportuno
+[ ] b) Reutilizável
+[X] c) Confiável
+[ ] d) Natural
+
+Explicação: A característica descrita é a confiabilidade (sistema confiável). Graças ao encapsulamento da POO, os detalhes
+internos de como uma classe realiza sua tarefa ficam isolados. Isso significa que você pode modificar ou corrigir a lógica
+interna de um metodo sem quebrar outras partes do sistema que dependem dele, garantindo estabilidade e segurança nas alterações.'''
