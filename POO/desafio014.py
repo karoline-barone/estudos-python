76e07287b28dfd0a15aaa2fd7a4b3473754d8c6f
Karoline <karol.barone@hotmail.com>
2026-08-24 08:59:53 -0300

    Desafio014 - Estado

---
+++ POO/desafio014.py
@@ -0,0 +1,8 @@
+'''Desafio 014 - Ao conjunto de todos os valores de atributos de um objeto em um determinado momento, damos o nome de:
+[ ] a) identidade
+[ ] b) instância
+[ ] c) método
+[X] d) estado
+
+Explicação: O estado de um objeto é definido pelas características ou valores atuais que as suas variáveis internas (atributos)
+possuem em um momento específico do tempo. Quando um método altera um atributo, ele muda o estado do objeto.'''
