0cab49785c016fee096f4efefec4dd9d9f9ce5ad
Karoline <karol.barone@hotmail.com>
2026-08-24 08:53:39 -0300

    Desafio012 - Instanciar

---
+++ POO/desafio012.py
@@ -0,0 +1,9 @@
+'''Desafio 012 - Na teoria de OO, o termo usado para o ato de fazer um objeto existir a partir de um modelo se chama:
+[X] a) instanciar
+[ ] b) classificar
+[ ] c) objetificar
+[ ] d) Forjar
+
+Explicação: Na Programação Orientada a Objetos, a classe funciona como um molde ou modelo. O ato de criar um objeto real
+baseado nesse modelo é chamado de instanciação (ou instanciar uma classe). Cada objeto criado passa a ser uma instância
+viva daquela classe na memória do computador.'''
