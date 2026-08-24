174258685d2089371e28da22e9999b572e89a76e
Karoline <karol.barone@hotmail.com>
2026-08-24 08:56:29 -0300

    Desafio013 - Métodos

---
+++ POO/desafio013.py
@@ -0,0 +1,9 @@
+'''Desafio 013 - Todo objeto tem uma lista de atividades que podem ser feitas com ele ou que ele pode desempenhar sozinho, que são:
+[ ] a) atributos
+[X] b) métodos
+[ ] c) classes
+[ ] d) instâncias
+
+Explicação: As ações, comportamentos ou atividades que um objeto pode realizar na Programação Orientada a Objetos são chamadas
+de métodos. No código, os métodos são estruturados como funções que pertencem a uma classe específica e operam sobre os dados
+(atributos) daquele objeto.'''
