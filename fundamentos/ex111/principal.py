f4535bb7d75b052ecb5f1a45033fff084c2f52f1
Karoline <karol.barone@hotmail.com>
2026-09-22 09:00:15 -0300

    Ex111 - Transformando módulos em pacotes

--- fundamentos/ex111/principal.py
+++ fundamentos/ex111/principal.py
@@ -1,6 +1,6 @@
 '''Crie um pacote chamado utilidadesCeV que tenha dois módulos internos chamados moeda e dado. Transfira todas as funções
 utilizadas nos desafios 107, 108 e 109 para o primeiro pacote e mantenha tudo funcionando.'''
-from ex111 import moeda
+from ex111.utilidadescev import moeda
 
 valor = float(input('Digite um valor: R$ '))
 moeda.resumo(valor, 15, 10)
